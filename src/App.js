6cc0cd359499443db5d9dccfca65aae97e3bdfcc
Lukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2020-11-09 23:38:51 +0100

    global styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import ListView from './views/ListView/ListView';
 import NotFoundView from './views/NotFoundView/NotFoundView';
+import GlobalStyle from 'theme/GlobalStyle';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <>
+          <GlobalStyle />
           <Switch>
             <Route exact path="/" component={ListView} />
             <Route path="/notFound" component={NotFoundView} />
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat');
+
+    *, *::before, *::after {
+    box-sizing: border-box;
+    }
+
+    html {
+        font-size: 10px;
+    }
+
+    body {
+        font-size: 1.6rem;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+`;
+
+export default GlobalStyle;
